9c64144d27acaaa9aee6fe8a1cbe5297a18291ee
Tony Kingston <tony.kingston16@gmail.com>
2022-05-10 23:06:36 +0100

    Assimp integration.

--- Plugins/OpenGLRendering/Model.h
+++ Plugins/OpenGLRendering/Model.h
@@ -1,8 +1,35 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "OGLMesh.h"
+#include "OGLShader.h"
+#include "OGLTexture.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 
 namespace NCL {
 	namespace Rendering {
+
+        using std::string;
+        using std::vector;
 		class Model {
+        public:
+            Model(char* path) {
+                loadModel(path);
+            }
+            void Draw(OGLShader& shader);
+        private:
+            // model data
+            vector<OGLMesh> meshes;
+            string directory;
+
+            void loadModel(string path);
+            void processNode(aiNode* node, const aiScene* scene);
+            OGLMesh processMesh(aiMesh* mesh, const aiScene* scene);
+            vector<OGLTexture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+                string typeName);
 		};
 	}
 }
