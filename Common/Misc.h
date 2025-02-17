ef8c5712c603fbb110165cb4352bd1c4dacf7f86
Tony Kingston <tony.kingston16@gmail.com>
2025-02-17 19:31:38 +0000

    Adding CRTP helper class. Updating Singleton so it can properly access protected constructors.

--- Common/Misc.h
+++ Common/Misc.h
@@ -2,8 +2,7 @@
 #include <Build.h>
 
 template<class T>
-class Singleton
-{
+class Singleton {
 public:
 	static T& Get() { 
 		static T* instance = new T();
@@ -16,15 +15,28 @@ public:
 	}
 
 protected:
+
 	Singleton() = default;
 	virtual ~Singleton() = default;
 
 private:
+
+	friend T;
 
 	Singleton(const Singleton&) = delete;
 	Singleton& operator=(const Singleton&) = delete;
 	Singleton(Singleton&&) = delete;
 	Singleton& operator=(Singleton&&) = delete;
 
-	void Reset() { instance->~T(); }
+	void Reset() { Get().~T(); }
+};
+
+// Inherit when using the curiously recurring template pattern (CRTP)
+template<typename T>
+class CRTP
+{
+public:
+	T& Impl() { return static_cast<T&>(*this); }
+	T const& Impl() const { return static_cast<T const&>(*this); }
+private:
 };
